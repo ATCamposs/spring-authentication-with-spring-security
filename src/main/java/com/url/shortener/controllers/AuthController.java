12bae5232295d2a310322facf2d83fd2ecbdedc0
Andre Campos <andre.tatibano@gmail.com>
2022-08-03 23:11:19 -0300

    remove domain from controller

--- src/main/java/com/url/shortener/controllers/AuthController.java
+++ src/main/java/com/url/shortener/controllers/AuthController.java
@@ -3,64 +3,37 @@ package com.url.shortener.controllers;
 import com.url.shortener.controllers.params.LoginParams;
 import com.url.shortener.controllers.params.UserRegisterParams;
 import com.url.shortener.dto.UserDto;
-import com.url.shortener.repositories.RoleRepository;
 import com.url.shortener.repositories.UserRepository;
-import com.url.shortener.security.jwt.JwtUtils;
-import com.url.shortener.security.services.UserDetailsImpl;
 import com.url.shortener.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.Instant;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    UserService userService;
-    @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    RoleRepository roleRepository;
-    @Autowired
-    PasswordEncoder encoder;
-    @Autowired
-    JwtUtils jwtUtils;
+
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping(path = "/sign_in", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> authenticateUser(@Valid @RequestBody LoginParams loginParams) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginParams.email, loginParams.password));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        var token = jwtUtils.generateTokenFromEmail(userDetails.getEmail());
-        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(token);
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
-                .body("Bearer " + token);
+    public ResponseEntity<UserDto> authenticateUser(@Valid @RequestBody LoginParams loginParams) {
+        var userDto = userService.loginByEmailAndPassword(loginParams.email, loginParams.password);
+        if (userDto == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        return ResponseEntity.ok().body(userDto);
     }
 
     @PostMapping(path = "/sign_up", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegisterParams userRegisterParams) {
-        if (userRepository.existsByEmail(userRegisterParams.email)) {
+        if (userRepository.existsByEmail(userRegisterParams.email))
             return ResponseEntity.badRequest().body(Map.of("message", "Email is already taken!"));
-        }
 
         var newUser = userService.createDefaultByParams(userRegisterParams);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
@@ -68,8 +41,6 @@ public class AuthController {
 
     @PostMapping(path = "/sign_out", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> logoutUser() {
-        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(Map.of("message", "You've been signed out!"));
+        return ResponseEntity.ok().body(Map.of("message", "You've been signed out!"));
     }
 }
--- src/main/java/com/url/shortener/services/UserService.java
+++ src/main/java/com/url/shortener/services/UserService.java
@@ -1,10 +1,11 @@
 package com.url.shortener.services;
 
 import com.url.shortener.controllers.params.UserRegisterParams;
+import com.url.shortener.dto.UserDto;
 import com.url.shortener.models.Role;
-import com.url.shortener.models.User;
 import com.url.shortener.repositories.RoleRepository;
 import com.url.shortener.repositories.UserRepository;
+import com.url.shortener.security.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,19 +19,44 @@ public class UserService {
     private final PasswordService passwordService;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final JwtUtils jwtUtils;
 
     @Transactional(rollbackOn = Exception.class)
-    public User createDefaultByParams(UserRegisterParams userRegisterParams) {
+    public UserDto createDefaultByParams(UserRegisterParams userRegisterParams) {
         userRegisterParams.password = passwordService.hash(userRegisterParams.password);
         var newUser = UserRegisterParams.MAPPER.toModel(userRegisterParams);
 
+        Set<Role> roles = getUserRoles();
+
+        newUser.setRoles(roles);
+        userRepository.save(newUser);
+        var user = userRepository.findByEmail(newUser.getEmail()).get();
+
+        var token = jwtUtils.generateTokenFromEmail(newUser.getEmail());
+        return UserDto.MAPPER.toDto(user, roles, token, "");
+    }
+
+    public UserDto loginByEmailAndPassword(String email, String password) {
+        var user = userRepository.findByEmail(email);
+        if (user.isEmpty()) return null;
+        var actualUser = user.get();
+        if (passwordService.verify(password, actualUser.getPassword())) {
+            var roles = roleRepository.findByUserEmail(email);
+            var token = jwtUtils.generateTokenFromEmail(email);
+            return UserDto.MAPPER.toDto(actualUser, roles, token, "");
+        }
+        return null;
+    }
+
+    public void removeRefreshToken(String email) {
+        //userRepository.removeUserRefreshToken();
+    }
+
+    private Set<Role> getUserRoles() {
         Set<Role> roles = new HashSet<>();
         Role userRole = roleRepository.findByName(Role.Type.USER)
                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
         roles.add(userRole);
-
-        newUser.setRoles(roles);
-
-        return userRepository.save(newUser);
+        return roles;
     }
 }

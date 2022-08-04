00460d8ebff17cd535c9f58129bbadd04c5159e8
Andre Campos <andre.tatibano@gmail.com>
2022-08-03 23:12:32 -0300

    get roles by user email

--- src/main/java/com/url/shortener/repositories/RoleRepository.java
+++ src/main/java/com/url/shortener/repositories/RoleRepository.java
@@ -2,10 +2,17 @@ package com.url.shortener.repositories;
 
 import com.url.shortener.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> findByName(Role.Type name);
+
+    @Query(value = """
+            SELECT r.id, r.name FROM roles r WHERE r.id IN (SELECT ur.role_id FROM user_roles ur WHERE ur.user_id = (SELECT u.id FROM users u WHERE u.email = ?1))
+            """, nativeQuery = true)
+    Set<Role> findByUserEmail(String email);
 }
